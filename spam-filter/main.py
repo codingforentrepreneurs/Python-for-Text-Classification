02f2a88a83d87f5e792105d2bdb32ba4945a7b69
CFE <hello@teamcfe.com>
2018-09-20 13:51:38 -0700

    Bag of Words

---
+++ spam-filter/main.py
@@ -0,0 +1,25 @@
+# bag of words
+
+
+txt = "This is awesome"
+
+txt2 = "This is bad"
+
+txt3 = "What a great time"
+
+bag_of_words = ["this", "is", "awesome", "bad", 'what', 'a', 'great', 'time']
+
+
+# this based on positions
+txt_bow = [0, 1, 2]
+
+txt2_bow = [0, 1, 3]
+
+txt3_bow = [4, 5, 6, 7]
+
+
+
+tx3_oha = [0, 0, 0, 0, 1, 1, 1, 1]
+
+
+
