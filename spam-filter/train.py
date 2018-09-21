056a6d4e962f68ac51cd0c70138f2ac6aa87dcf6
CFE <hello@teamcfe.com>
2018-09-20 18:31:34 -0700

    11 - Training Data and Labels as Numpy Arrays

--- spam-filter/train.py
+++ spam-filter/train.py
@@ -18,28 +18,61 @@ file_to_bow(filepath='data/simple/neg.txt')
 
 
 
-print(bag)
+#print(bag)
 
+'''
+Positive = label = 1
+Negative = label = 0
 
-def file_to_oha(filepath='data/simple/pos.txt'):
+'''
+
+def file_to_oha(filepath='data/simple/pos.txt', label=1):
     my_oha = []
+    labels = []
+    if filepath.endswith("neg.txt"):
+        label = 0
     with open(filepath, 'r') as f:
         lines = f.readlines()
         for line in lines:
             escaped_line = line.replace('\n', '')
             oha = to_one_hot(escaped_line)
+            labels.append(label)
             my_oha.append(oha)
-    return my_oha
+    return my_oha, labels
 
 
 
 
-pos_ohas = file_to_oha(filepath='data/simple/pos.txt')
-neg_ohas = file_to_oha(filepath='data/simple/neg.txt')
+pos_ohas, pos_labels = file_to_oha(filepath='data/simple/pos.txt')
+neg_ohas, neg_labels = file_to_oha(filepath='data/simple/neg.txt')
 
 
-print(pos_ohas, neg_ohas)
+#print(pos_ohas, pos_labels)
 
+import numpy as np
+X_pos = np.array(pos_ohas)
+X_neg = np.array(neg_ohas)
+
+y_pos = np.array(pos_labels)
+y_neg = np.array(neg_labels)
+
+X = np.concatenate((X_pos, X_neg), axis=0)
+y = np.concatenate((y_pos, y_neg), axis=0)
+
+
+#X = Training data
+#y = labels = target = this actual value of that trainin data
+
+print(X)
+print(y)
+print(len(X) == 14)
+print(len(y) == len(X))
+
+from sklearn.utils import shuffle
+
+X, y = shuffle(X, y, random_state=0)
+
+print(X,y)
 
 
 
