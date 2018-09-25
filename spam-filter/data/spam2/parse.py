f015b18d6a0212dabd94b4d62a7028a663103ab4
CFE <hello@teamcfe.com>
2018-09-25 15:04:50 -0700

    19 Grab and Parse Dataset

---
+++ spam-filter/data/spam2/parse.py
@@ -0,0 +1,34 @@
+import os
+
+ROOT_DIR = os.getcwd()
+RAW_PATH = os.path.join(ROOT_DIR,
+        'raw_src',
+        'smsspamcollection',
+        'SMSSpamCollection'
+    )
+
+NOT_SPAM_TXT = os.path.join(ROOT_DIR,
+        'not-spam.txt'
+    )
+
+SPAM_TXT = os.path.join(ROOT_DIR,
+        'spam.txt'
+    )
+
+not_spam_file   = open(NOT_SPAM_TXT, 'w')
+spam_file       = open(SPAM_TXT, 'w')
+with open(RAW_PATH, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        label, txt = line.split('\t')
+        if label == 'ham':
+            not_spam_file.write(txt)
+        if label == 'spam':
+            spam_file.write(txt)
+
+not_spam_file.close()
+spam_file.close()
+
+
+
+
