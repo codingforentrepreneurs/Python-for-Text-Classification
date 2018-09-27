3134d46fce088fd2d191f6a26f81289ccc843819
CFE <hello@teamcfe.com>
2018-09-27 12:17:10 -0700

    27 - Convert Our Text Data into Sequences

--- spam-filter/vectors.py
+++ spam-filter/vectors.py
@@ -9,13 +9,37 @@ bag = []
 ROOT_DIR            = os.getcwd()
 SPAM_DATA_PATH      = os.path.join(ROOT_DIR, 'data', 'spam', 'spam.txt')
 NOT_SPAM_DATA_PATH  = os.path.join(ROOT_DIR, 'data', 'spam', 'not-spam.txt')
+spam_file           = open(SPAM_DATA_PATH, 'r')
+spam_lines          = spam_file.readlines()
+not_spam_file       = open(NOT_SPAM_DATA_PATH, 'r')
+not_spam_lines       = not_spam_file.readlines()
+
+
 SPAM_LABEL          = 0
 NOT_SPAM_LABEL      = 1
 MAX_LINES           = 500
-MAX_NUM_WORDS       = 1000
-MAX_SEQUENCE_LENGTH = 10
+MAX_NUM_WORDS       = 20000
+MAX_SEQUENCE_LENGTH = 1000 # important 
 
-texts = ["this is awesome", "this is not great", "this is cool!"]
+texts = []
+labels = []
+
+for line in not_spam_lines:
+    texts.append(line)
+    labels.append('not-spam')
+
+for line in spam_lines:
+    texts.append(line)
+    labels.append('spam')
+
+
+spam_file.close()
+not_spam_file.close()
+
+
+
+
+
 tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
 tokenizer.fit_on_texts(texts)
 
@@ -43,7 +67,8 @@ print(sequences)
 '''
 data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
 
-print(data)
+print(labels)
+#print(data)
 
 
 
