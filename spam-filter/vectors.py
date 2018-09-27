445ee6b0d66f7d0ca7a9678764fa01d7a48a9ed0
CFE <hello@teamcfe.com>
2018-09-27 13:19:07 -0700

    29 - Reusable Text-Label Utility

--- spam-filter/vectors.py
+++ spam-filter/vectors.py
@@ -13,76 +13,44 @@ bag = []
 ROOT_DIR            = os.getcwd()
 SPAM_DATA_PATH      = os.path.join(ROOT_DIR, 'data', 'spam', 'spam.txt')
 NOT_SPAM_DATA_PATH  = os.path.join(ROOT_DIR, 'data', 'spam', 'not-spam.txt')
-spam_file           = open(SPAM_DATA_PATH, 'r')
-spam_lines          = spam_file.readlines()
-not_spam_file       = open(NOT_SPAM_DATA_PATH, 'r')
-not_spam_lines       = not_spam_file.readlines()
-
-
-SPAM_LABEL          = 0
-NOT_SPAM_LABEL      = 1
 MAX_LINES           = 500
 MAX_NUM_WORDS       = 20000
 MAX_SEQUENCE_LENGTH = 1000 # important 
 
-texts = []
-labels = []
 
-for line in not_spam_lines:
-    texts.append(line)
-    labels.append('not-spam')
+def split_textfiles(paths=[], limit=10000):
+    texts = []
+    labels = []
+    for path in paths:
+        with open(path, 'r') as f:
+            filename = os.path.basename(path) # f.name
+            label, ext = os.path.splitext(filename)
+            lines = f.readlines()[:limit]
+            for line in lines:
+                # clean line text
+                texts.append(line)
+                labels.append(label)
+    return texts, labels
 
-for line in spam_lines:
-    texts.append(line)
-    labels.append('spam')
 
-spam_file.close()
-not_spam_file.close()
-
-
+texts, labels = split_textfiles(paths=[
+        SPAM_DATA_PATH,
+        NOT_SPAM_DATA_PATH,
+], limit=10000)
 
 
 
 tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
 tokenizer.fit_on_texts(texts)
-
-'''
-print(tokenizer.document_count)
-print(tokenizer.num_words)
-print(tokenizer.word_counts)
-print(tokenizer.word_docs)
-print(tokenizer.word_index)
-
-{'this': 1, 'is': 2, 'awesome': 3, 'not': 4, 'great': 5, 'cool': 6}
-'''
-
 sequences = tokenizer.texts_to_sequences(texts)
-
-'''
-print(sequences)
-
-[[1, 2, 3], [1, 2, 4, 5], [1, 2, 6]]
-
-
-[1, 2, 3]
-[1, 2, 4, 5]
-[1, 2, 6]
-'''
 data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-
-#print(labels)
-#print(data)
-
 encoder = LabelEncoder()
 encoder.fit(labels)
 encoded_Y = encoder.transform(labels)
 
 y = to_categorical(encoded_Y)
 
-
-# class_prediction = encoder.inverse_transform([0, 1, 2])
-# print(class_prediction)
-
+print(data, y)
 
 
 
