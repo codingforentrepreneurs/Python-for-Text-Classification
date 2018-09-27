4b0a5c65440b25dc6951a47af37eacb58c3c41d3
CFE <hello@teamcfe.com>
2018-09-27 12:12:15 -0700

    26 - Pad Sequences

--- spam-filter/vectors.py
+++ spam-filter/vectors.py
@@ -13,25 +13,40 @@ SPAM_LABEL          = 0
 NOT_SPAM_LABEL      = 1
 MAX_LINES           = 500
 MAX_NUM_WORDS       = 1000
-
+MAX_SEQUENCE_LENGTH = 10
 
 texts = ["this is awesome", "this is not great", "this is cool!"]
 tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
 tokenizer.fit_on_texts(texts)
 
-#print(dir(tokenizer))
-
+'''
 print(tokenizer.document_count)
 print(tokenizer.num_words)
 print(tokenizer.word_counts)
 print(tokenizer.word_docs)
 print(tokenizer.word_index)
 
-
-
-
+{'this': 1, 'is': 2, 'awesome': 3, 'not': 4, 'great': 5, 'cool': 6}
+'''
 
 sequences = tokenizer.texts_to_sequences(texts)
 
-#print(sequences)
+'''
+print(sequences)
+
+[[1, 2, 3], [1, 2, 4, 5], [1, 2, 6]]
+
+
+[1, 2, 3]
+[1, 2, 4, 5]
+[1, 2, 6]
+'''
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+
+print(data)
+
+
+
+
+
 
